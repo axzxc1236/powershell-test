d38b2862f5b700f1f39c922995c80d708cc81046
axzxc1236 <axzxc1236@gmail.com>
2019-01-04 22:21:01 +0800

    Create generate-random-password.ps1
    First powershell script I written.

---
+++ generate-random-password/generate-random-password.ps1
@@ -0,0 +1,20 @@
+﻿if (!$character_table) {
+    Set-Variable character_table -Option Constant -value "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789``~!@#$%^&*()_-+=\|[]{}',./<>?"""
+}
+# ` is escape character, so there are two ` in $character_table
+Function Get-NewPassword() {
+    param (
+      [int] $length
+    )
+    $output = ""
+    if (!$length) {
+        $length=10
+    }
+    for ($i=0; $i -lt $length; $i++) {
+        $random = Get-Random -Minimum 0 -Maximum $character_table.Length
+        $output += $character_table.Substring($random, 1)
+    }
+    return $output
+}
+
+Write-Host (Get-NewPassword 26)
