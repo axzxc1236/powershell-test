ac4d63ee0ddb38fc6f8bc9f6c593355ee133c18d
axzxc1236 <axzxc1236@gmail.com>
2019-01-04 23:02:21 +0800

    apply auto-format

--- generate-random-password/generate-random-password.ps1
+++ generate-random-password/generate-random-password.ps1
@@ -1,20 +1,16 @@
-﻿if (!$character_table) {
-	Set-Variable character_table -Option Constant -value "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789``~!@#$%^&*()_-+=\|[]{}',./<>?"""
-}
+﻿if (!$character_table) {Set-Variable -Name character_table -Option Constant -Value "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789``~!@#$%^&*()_-+=\|[]{}',./<>?"""}
 # ` is escape character, so there are two ` in $character_table
 Function Get-NewPassword() {
 	param (
 		[int] $length
 	)
 	$output = ""
-	if (!$length) {
-		$length=10
-	}
-	for ($i=0; $i -lt $length; $i++) {
+	if (!$length) {$length = 10}
+	for ($i = 0; $i -lt $length; $i++) {
 		$random = Get-Random -Minimum 0 -Maximum $character_table.Length
 		$output += $character_table.Substring($random, 1)
 	}
 	return $output
 }
 
-Write-Host (Get-NewPassword 26)
+Write-Host -Object (Get-NewPassword -length 26)
